ab656f874505ddc94c8e4eb200f9d6380ca59097
Aryan Singh <aryan.singh.lamba@gmail.com>
2023-10-10 18:04:45 -0700

    generateRating endpoint to post

--- src/api/edit/routes/01-custom-edit.js
+++ src/api/edit/routes/01-custom-edit.js
@@ -1,7 +1,7 @@
 module.exports = {
   routes: [
     {
-      "method": "GET",
+      "method": "POST",
       "path": "/generate-rating",
       "handler": "edit.generateRating",
       "config": {
